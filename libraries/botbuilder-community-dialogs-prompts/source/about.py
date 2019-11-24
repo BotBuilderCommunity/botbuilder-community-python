ae19e0451b107b0a5bd7b832165e5e53098fc7d2
Gary Pretty <gary@garypretty.co.uk>
2019-11-24 05:43:05 +0000

    Fixed repo url in about.py

--- libraries/botbuilder-community-dialogs-prompts/source/about.py
+++ libraries/botbuilder-community-dialogs-prompts/source/about.py
@@ -4,7 +4,7 @@ __title__ = "botbuilder-community-dialogs-prompts"
 __version__ = (
     os.environ["packageVersion"] if "packageVersion" in os.environ else "0.1.0"
 )
-__uri__ = "https://www.github.com/Microsoft/botbuilder-python"
+__uri__ = "https://github.com/BotBuilderCommunity/botbuilder-community-python"
 __author__ = "Bot Builder Community"
 __description__ = "Microsoft Bot Builder Community Dialogs Prompts"
 __summary__ = "Dialogs Prompts from the Bot Builder Community for Microsoft Bot Builder SDK for Python"
